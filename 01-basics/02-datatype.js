5197dc2a2c4f1c0979bf730faa2b2d3fe24533a3
mohammad nishad <mohammadnishad1243.mn@gmail.com>
2023-09-27 12:20:50 +0530

    data type

---
+++ 01-basics/02-datatype.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js code as newer version
+
+// alert("hello") // we are using node.js, not browser
+
+console.log(3
+     +
+      3)//code readability should be high
+
+console.log("nishad")
+
+
+let name ="nishad"//string -> ""
+let age = 18//number -> 2 to power 53 //bigint
+let isloggedIn = false //boolean -> true/false
+//null -> standalone value/ type->object
+//undefine ->value not assigned
+//symbol -> unique
+
+
+//object 
+
+console.log(typeof null);
